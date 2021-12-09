780aff35104c9bd90b36f72b6b5b871438378872
germansayago <german.sayago@gmail.com>
2021-12-09 15:30:09 -0300

    was added a navbar wich tailwind

---
+++ app/Http/Livewire/Navigation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use Livewire\Component;
+
+class Navigation extends Component
+{
+    public function render()
+    {
+        $categories = Category::all();
+
+        return view('livewire.navigation', compact('categories'));
+    }
+}
